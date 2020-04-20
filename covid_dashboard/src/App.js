eb0b55f38f5b30a055bc68f8d396f2cb6b901088
deepalisrivas <deepali.srivastava@tothenew.com>
2020-04-20 23:35:55 +0530

    App.js file

---
+++ covid_dashboard/src/App.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Main_page from './Main_page/Main_page';
+import Navbar from './Navbar/Navbar';
+import './App.css';
+
+// import Line from './Charts/Line-chart';
+import Faq from './FAQ/Faq';
+import Helpful from './Helpful_Links/Helpful_links'
+
+
+
+class App extends Component {
+  
+
+  
+  render() {
+    return (
+      <Router> 
+      <div className="App">
+      <div>
+          <Navbar />
+      </div>
+          <Route path="/" exact component={Main_page}/>
+          <Route path="/FAQ" component={Faq}/>
+          <Route path="/HELPFUL_LINKS" component={Helpful}/>
+    
+      </div>
+      </Router> 
+      
+    );
+  }
+}
+
+
+export default App
+ 
